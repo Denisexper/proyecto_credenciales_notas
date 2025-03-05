cbdc51ed1d507dae3ff38788b8af08fa17eafad6
Denisexper <clasic.exper2023@gmail.com>
2025-03-05 16:26:10 -0600

    feat: notas_bussiness

--- CapaNegocio/NotasNegocio.cs
+++ CapaNegocio/NotasNegocio.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CapaDatos;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace CapaNegocio
 {
     public class NotasNegocio
     {
+        NotasDatos DatosNotas = new NotasDatos();
+
+        public DataTable ObtenerNotas()
+        {
+            return DatosNotas.ObtenerNotas();
+        }
     }
 }
